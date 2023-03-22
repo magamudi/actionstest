e5fcec0ea298f027646a2a24897845e678bc2637
magnusamudi <magnusamudi@gmail.com>
2023-03-22 03:11:03 +0000

    seventh commit

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-west-1"
 }
 
-resource "aws-vpc" "actions {
+resource "aws_vpc" "actions" {
     cidr_block = 10.0.0.0/16
     
-    tags = {
-        Name = "Class30"
+tags = {
+    Name = "Class30"
     }
 }
