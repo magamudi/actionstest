25da06ae3051b1cbb904153bfd6bc0094f4774b8
magnusamudi <magnusamudi@gmail.com>
2023-03-26 03:07:02 +0000

    feat: backend

--- main.tf
+++ main.tf
@@ -17,8 +17,5 @@ terraform {
     key    = "prod/terraform.tfstate"
     region = "us-east-2"
     # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
-  }
 }
 
