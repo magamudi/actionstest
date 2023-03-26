99bd3f4fdf1a3d84747c4ed1c7181196be2cf14a
magnusamudi <magnusamudi@gmail.com>
2023-03-26 03:02:37 +0000

    feat: backend

--- main.tf
+++ main.tf
@@ -9,3 +9,16 @@ tags = {
     Name = "Class30"
     }
 }
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "terraform-state-magnus"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
